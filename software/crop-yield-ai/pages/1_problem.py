17f5aae2dc758608a3ac6a2c55fed4ceda25c3f2
benkimz <benkim3619@gmail.com>
2023-02-26 06:18:42 +0300

    crop-yield-ai project push

---
+++ software/crop-yield-ai/pages/1_problem.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":star:", 
+    page_title="Problem Statement"
+)
+
+st.header("Problem Statement")
+st.markdown(
+    """
+    
+    Developing a machine learning model to predict crop yields in Sub-Saharan Africa based on 
+    various factors such as weather patterns, soil quality, irrigation systems, and farming techniques. 
+    This will help farmers make informed decisions regarding crop planting and harvesting, 
+    which can improve food security, reduce waste, and increase income for farmers. Predicting crop 
+    yields in Sub-Saharan Africa.
+    
+    """
+)
